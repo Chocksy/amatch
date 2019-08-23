cd586b996da1f1f4dc2e53885d404f7192a23a08
Ciocanel Razvan <chocksy@gmail.com>
2019-08-23 20:44:15 +0300

    create a map class to hold multiple needles

--- lib/amatch.rb
+++ lib/amatch.rb
@@ -1,3 +1,21 @@
+# frozen_string_literal: true
+
+# include the extension
+require 'amatch_ext'
+# include gem files
+require 'amatch/version'
+require 'amatch/string'
+require 'amatch/map'
+
 module Amatch
+  DiceCoefficient = PairDistance
+  DEFAULT_ALGORITHM = 'PairDistance'
+
+  def self.reset(algorithm = DEFAULT_ALGORITHM)
+    @map = Map.new(algorithm)
+  end
+
+  def self.map(algorithm = DEFAULT_ALGORITHM)
+    @map ||= Map.new(algorithm)
+  end
 end
-require 'amatch/rude'
---
+++ lib/amatch/map.rb
@@ -0,0 +1,38 @@
+module Amatch
+  class Map
+    attr_reader :patterns, :algorithm
+
+    def initialize(algo_class)
+      @patterns = {}
+      @algorithm = "Amatch::#{algo_class.to_s.camelize}".constantize
+    end
+
+    def add(needle, reference)
+      needle = normalize_string(needle)
+      patterns[reference] = algorithm.new(needle)
+    end
+
+    def find(needle)
+      needle = normalize_string(needle)
+      finds = []
+      patterns.each do |key, pattern|
+        finds << [pattern.match(needle), key]
+      end
+      finds.sort_by { |(weight, _key)| weight }.reverse
+    end
+
+    def delete(key)
+      patterns.delete!(key)
+    end
+
+    private
+
+    def normalize_string(needle)
+      result = needle.downcase
+      unless result =~ /^([a-z ])+$/
+        result = result.unicode_normalize(:nfd).gsub(/[^\x00-\x7F]/,'').to_s.gsub(/[^a-z]/,' ')
+      end
+      result.gsub(/\s+/,' ').strip
+    end
+  end
+end
