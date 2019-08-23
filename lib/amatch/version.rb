8c85a670e25012fa117635ee82ee63a2eb7674a7
Ciocanel Razvan <chocksy@gmail.com>
2019-08-23 21:34:07 +0300

    [version] bump version to 0.4.1

--- lib/amatch/version.rb
+++ lib/amatch/version.rb
@@ -1,8 +1,5 @@
+# frozen_string_literal: true
 module Amatch
   # Amatch version
-  VERSION         = '0.4.0'
-  VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
-  VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
-  VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
-  VERSION_BUILD   = VERSION_ARRAY[2] # :nodoc:
+  VERSION         = '0.4.1'.freeze
 end
