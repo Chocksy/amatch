2279ca79f272182a6a587111fa5a0c96f913736e
Ciocanel Razvan <chocksy@gmail.com>
2019-08-23 21:23:45 +0300

    update the string class to include string methods

--- lib/amatch/string.rb
+++ lib/amatch/string.rb
@@ -1,12 +1,3 @@
 class ::String
-  def camelize
-    split("_").map { |part|
-      part[0] = part[0].upcase
-      part
-    }.join
-  end
-
-  def constantize
-    split('::').inject(Kernel, &:const_get)
-  end
+  include ::Amatch::StringMethods
 end
